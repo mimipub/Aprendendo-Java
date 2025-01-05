41bbeb119b3ec7bf3cf30c1af43e2272954ceff7
Mizael <Mizaelmiguel246@gmail.com>
2025-01-05 15:43:46 -0300

    Imprimindo Hello World! em Java

---
+++ aula2/Variavel.java
@@ -0,0 +1,31 @@
+package aula2;
+
+public class Variavel {
+    public static void main(String[] args) {
+        
+        String nome = "João";
+        int idade = 25;
+        boolean isEstudante = true;
+        double altura = 1.75;
+        
+        System.out.println("meu nome é " + nome + " e tenho " + idade + " anos com altura de " + altura + " sou estudante? " + isEstudante);
+
+
+        Long numero1 = 7277777777L;
+        System.out.println(numero1);
+        
+
+        String nome1 = "Mizael"; // String armazena texto.
+        String nome2 = "Miguel";
+        String juntos = nome1 +" "+ nome2;
+        System.out.println(nome1 +" "+ nome2);
+        System.out.println(juntos);
+    
+    // char
+
+    char simbol = 'J';  // char armazena apenas um caractere.
+    System.out.println("simbolo: " + simbol);
+
+    
+    }
+}
---
+++ aula2/if_Else.java
@@ -0,0 +1,21 @@
+package aula2;
+
+import java.util.Scanner;
+import java.util.Locale;
+public class if_Else {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        double b = sc.nextDouble();
+        String c = sc.next();
+
+        sc.nextLine();
+        
+        System.out.println("String: " + c);
+        System.out.printf("Double: %.4f%n", b);
+        System.out.println("Int: " + a);
+        sc.close();
+    }
+}
