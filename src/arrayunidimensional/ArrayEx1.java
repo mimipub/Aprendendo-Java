5b96520ad162295c68a279c3de2b1c70f6cddf31
Mizael <Mizaelmiguel246@gmail.com>
2025-05-08 19:38:00 -0300

    :sparkles: feat(arrayEx1) praticando array unidimensional com objetos.

---
+++ src/arrayunidimensional/ArrayEx1.java
@@ -0,0 +1,32 @@
+package arrayunidimensional;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ArrayEx1 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Product[] array = new Product[n];
+
+        for (int i = 0; i < n; i++) {
+            sc.nextLine();
+            String name = sc.next();
+            double price = sc.nextDouble();
+            array[i] = new Product(name, price);
+        }
+
+        double total = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += array[i].getPrice();
+        }
+        double average = total / n;
+
+        System.out.println(" Average price " + average);
+
+
+        sc.close();
+    }
+}
