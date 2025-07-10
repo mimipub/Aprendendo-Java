5eb4c6a5b76b23c4d71424f364e35bae2082c475
Mizael <Mizaelmiguel246@gmail.com>
2025-07-10 14:25:45 -0300

    :sparkles: feat(HourContract) Criando classe principal HourContract

---
+++ src/composicao/Exercicio1/HourContract.java
@@ -0,0 +1,47 @@
+package composicao.Exercicio1;
+
+import java.util.Date;
+
+
+public class HourContract {
+    private Date date;
+    private Double ValuePerHour;
+    private Integer Hours;
+
+    public HourContract() {
+    }
+
+    public HourContract(Date date, Double valuePerHour, Integer hours) {
+        this.date = date;
+        ValuePerHour = valuePerHour;
+        Hours = hours;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Double getValuePerHour() {
+        return ValuePerHour;
+    }
+
+    public void setValuePerHour(Double valuePerHour) {
+        ValuePerHour = valuePerHour;
+    }
+
+    public Integer getHours() {
+        return Hours;
+    }
+
+    public void setHours(Integer hours) {
+        Hours = hours;
+    }
+
+    public double totalvalue(){
+        return ValuePerHour * Hours;
+    }
+}
