1714eb52f8c0d84d55e9c4c2c94be6e38f85b72f
Mizael <Mizaelmiguel246@gmail.com>
2025-08-11 13:30:38 -0300

    :sparkles: feat(Program) Tratamento de exceções

---
+++ src/trycatch/program.java
@@ -0,0 +1,26 @@
+package trycatch;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try{
+
+            System.out.println("Digite um inteiro:");
+            int numerador = sc.nextInt();
+            System.out.println("Digite um divisor:");
+            int denominador = sc.nextInt();
+
+            double div = numerador/denominador;
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Erro: Divisão por zero não é permitida.");
+        }
+        catch (InputMismatchException e){
+            System.out.println("Digite um número inteiro válido.");
+        }
+        sc.close();
+    }
+}
