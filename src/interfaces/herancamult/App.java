a444fa34a06e9a1eea62370803ec60195b652db3
Mizael <Mizaelmiguel246@gmail.com>
2025-09-19 13:16:32 -0300

    :sparkles: feat(Interface) Implementando interface

--- src/interfaces/herancamult/App.java
+++ src/interfaces/herancamult/App.java
@@ -2,12 +2,21 @@ package interfaces.herancamult;
 
 public class App {
     public static void main(String[] args) {
-        Printer p = new Printer("1080");
-        p.processDoc("my Letter");
-        p.Printer("my letter");
 
-        Scanner sc = new Scanner("8080");
+        System.out.println();
+        Printe p = new Printe("1080");
+        p.processDoc("my Letter");
+        p.Printe("my letter");
+
+        System.out.println();
+        Scaner sc = new Scaner("8080");
         sc.processDoc("my Letter");
         System.out.println("Scan Result " + sc.scan());
+
+        System.out.println();
+        ComboDevice cd = new ComboDevice("2003");
+        cd.processDoc("My dissertation");
+        cd.print("My dissertation");
+        System.out.println("Combo printing " + cd.scan());
     }
 }
